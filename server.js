bb68a3f8606002bc2bb944b9b5c56d8ca24c4418
hazem <livehazem@gmail.com>
2024-10-25 13:51:23 +0200

    edir port

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 app.use('/hero', heroRoute);
 
-
-app.listen(process.env.port || 3000 , ()=>{
+const PORT = process.env.PORT || 3000
+app.listen(PORT , ()=>{
     console.log('server work');
 })
